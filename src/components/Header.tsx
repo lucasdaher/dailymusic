637a47f8764cecd997cd0df13be994f4708c6b43
lucasdaher <contato@lucasdaher.com>
2024-08-21 11:10:09 -0300

    feat: adicionando estrutura de pastas para componentes e views.

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface HeaderProps {}
+
+export default function Header({}: HeaderProps) {
+  return <React.Fragment></React.Fragment>;
+}
---
+++ src/view/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface HomeProps {}
+
+export default function Home({}: HomeProps) {
+  return <React.Fragment></React.Fragment>;
+}
