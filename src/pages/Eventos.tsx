bcf9de380d77d1da498130857c68a0add0179b43
lucasdaher <contato@lucasdaher.com>
2024-09-28 17:47:34 -0300

    adicionando eventos na aba de eventos.

--- src/pages/Eventos.tsx
+++ src/pages/Eventos.tsx
@@ -4,6 +4,9 @@ import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 
 import festaSaoMiguel from "../assets/events/festaSaoMiguel.png";
+import sollares from "../assets/events/sollares.png";
+import ixquece from "../assets/events/ixquece.png";
+import apoena from "../assets/events/apoena.png";
 
 export default function Eventos() {
   const eventos = [
@@ -17,27 +20,27 @@ export default function Eventos() {
     },
     {
       id: 2,
-      nome: "Festa de São Miguel Arcanjo 2024",
-      loc: "Centro de Eventos do Ceará - Fortaleza",
-      image: festaSaoMiguel,
-      alt: "",
-      link: "",
+      nome: "Sollares Sunset - Vintage Culture",
+      loc: "Centro de Convenções Salvador, Salvador - BA",
+      image: sollares,
+      alt: "Capa do Sollares Sunset com Vintage Culture",
+      link: "https://www.sympla.com.br/evento/sollares-sunset-vintage-culture/2620100",
     },
     {
       id: 3,
-      nome: "Festa de São Miguel Arcanjo 2024",
-      loc: "Centro de Eventos do Ceará - Fortaleza",
-      image: festaSaoMiguel,
-      alt: "Capa de Festa de São Miguel",
-      link: "",
+      nome: "Ixquece | Carnaval 2025",
+      loc: "Espaço Galeria Jardim, Rio de Janeiro - RJ",
+      image: ixquece,
+      alt: "Capa da festa de carnaval Ixquece",
+      link: "https://www.sympla.com.br/evento/ixquece-l-carnaval-2025/2633645",
     },
     {
       id: 4,
-      nome: "Festa de São Miguel Arcanjo 2024",
-      loc: "Centro de Eventos do Ceará - Fortaleza",
-      image: festaSaoMiguel,
-      alt: "Capa de Festa de São Miguel",
-      link: "",
+      nome: "3º Festival Apoena - A Música da Floresta",
+      loc: "Nabêra Gastronomia e Cultura, Belém - PA",
+      image: apoena,
+      alt: "Capa do 3º Festival Apoena - A Música da Floresta",
+      link: "https://www.sympla.com.br/evento/3-festival-apoena-a-musica-da-floresta/2588999",
     },
   ];
   return (
@@ -47,11 +50,11 @@ export default function Eventos() {
       <main className="mt-32 max-w-[1440px] w-full mx-auto mb-32">
         <div className="flex flex-col justify-center items-center gap-2 w-full h-96 background-presentation rounded-lg">
           <h2 className="text-white font-semibold text-4xl">
-            Eventos musicais no Brasil nos próximos dias
+            Eventos musicais que acontecerão no Brasil
           </h2>
           <p className="text-white/60 font-normal text-xl">
-            Selecionamos alguns eventos que irão acontecer no Brasil ao decorrer
-            do mês.
+            Selecionamos alguns eventos que estão agendados para acontecer neste
+            e no próximo ano.
           </p>
           <Link
             to={`/`}
@@ -61,16 +64,20 @@ export default function Eventos() {
           </Link>
         </div>
 
-        <div className="grid grid-cols-3 justify-center content-center gap-8 mt-12">
+        <div className="grid grid-cols-3 justify-center content-center gap-y-8 mt-12">
           {eventos.map((evento) => (
             <a href={evento.link} target="_blank" rel="noopener noreferrer">
               <div
-                className="flex flex-col justify-center items-start gap-4 p-8 hover:bg-black/20 transition-all duration-200 rounded-lg"
+                className="flex flex-col justify-center items-start gap-4 p-8 hover:bg-malibu-100/10 transition-all duration-200 rounded-lg"
                 key={evento.id}
               >
                 <picture>
-                  <source type="image/png+xml" srcSet={festaSaoMiguel} />
-                  <img src={festaSaoMiguel} alt={evento.alt} />
+                  <source type="image/png+xml" srcSet={evento.image} />
+                  <img
+                    src={evento.image}
+                    alt={evento.alt}
+                    className="rounded-2xl w-full"
+                  />
                 </picture>
 
                 <div className="flex flex-col justify-center items-start gap-1">
