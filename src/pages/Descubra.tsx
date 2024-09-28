4ce3dfd2480a8b2ca8bd90fd3e7faf9a5987cd30
lucasdaher <contato@lucasdaher.com>
2024-09-28 00:56:42 -0300

    corrigindo roteamento.

--- src/pages/Descubra.tsx
+++ src/pages/Descubra.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 interface DescubraProps {}
 
 export default function Descubra({}: DescubraProps) {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
 }
