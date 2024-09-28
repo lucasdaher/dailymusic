380a0202943ae1a53e617000673687ce38e6adda
lucasdaher <contato@lucasdaher.com>
2024-09-28 00:26:44 -0300

    atualizando caminho para nova logo.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import dailymusicLogo from "../assets/logo/dailymusic.svg";
+import dailymusicLogo from "../assets/logo/dailyMusicLogo.svg";
 
 export default function Header() {
   return (
